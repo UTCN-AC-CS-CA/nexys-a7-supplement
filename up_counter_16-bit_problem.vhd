05ae39c9d34ec1794fb2bffff2a55839ca2f711b
Attila Füzes <Attila.Fuzes@cs.utcluj.ro>
2025-03-05 22:39:10 +0200

    Added 16-bit counter

---
+++ up_counter_16-bit_problem.vhd
@@ -0,0 +1,37 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.std_logic_arith.all;
+  use ieee.std_logic_unsigned.all;
+
+entity proba1 is
+  port (
+    clk : in  std_logic;
+    btn : in  std_logic_vector(4  downto 0);
+    sw  : in  std_logic_vector(15 downto 0);
+    led : out std_logic_vector(15 downto 0);
+    an  : out std_logic_vector(7  downto 0);
+    cat : out std_logic_vector(6  downto 0)
+  );
+end entity proba1;
+
+architecture behavioral of proba1 is
+
+  signal en  : std_logic                     := '0';
+  signal cnt : std_logic_vector(15 downto 0) := (others => '0');
+
+begin
+
+  en <= btn(0);
+
+  process(clk) 
+  begin
+    if rising_edge(clk) then
+      if en = '1' then 
+        cnt <= cnt + 1; 
+      end if;
+    end if;
+  end process;
+  
+  led <= cnt;
+
+end architecture behavioral; 
