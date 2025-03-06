067e984f7df3b20ba4602b596a95018ae617644d
Attila Füzes <attila.fuzes@cs.utcluj.ro>
2025-03-06 09:12:25 +0200

    Update mono_pulse_gen.vhd

--- mono_pulse_gen.vhd
+++ mono_pulse_gen.vhd
@@ -27,5 +27,30 @@ begin
   end process;
   
   s_en_in <= '1' when s_cnt_out = x"000F" else '0';
+  
+  process(clk, s_en_in)
+  begin
+    if rising_edge(clk) then
+      if s_en_in = '1' then
+        s_dff1_out <= btn;
+      end if;
+    end if;
+  end process;
+  
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      s_dff2_out <= s_dff1_out;
+    end if;
+  end process;
+  
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      s_dff3_out <= s_dff2_out;
+    end if;
+  end process;
+  
+  enable <= s_dff2_out and not s_dff3_out;
 
 end behavioral; 
